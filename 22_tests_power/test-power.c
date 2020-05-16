7ef70757b32caea0dcb84466d483ba672eb1130a
Coursera Learner <nobody@nowhere.nul>
2020-05-16 05:43:47 +0000

    Did assignment 22

---
+++ 22_tests_power/test-power.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+ #include<stdlib.h>
+
+ unsigned power(unsigned x,unsigned y);
+
+int main()
+{
+  if( power(0,0) != 1)
+    {printf("broken1  implementation");
+      exit(EXIT_FAILURE);}
+  
+  if(power(30,0) != 1)
+    {printf("broken2 implementation");
+    exit(EXIT_FAILURE);}
+  
+  if(power(0,1) != 0)
+    {printf("broken3 implementation");
+      exit(EXIT_FAILURE);}
+  
+   if(power(45,1) != 45)
+     {printf("broken4 implementation");
+	  exit(EXIT_FAILURE);}
+   
+   if(power(2,3)== power(3,2))
+     {printf("broken5 implementation");
+       exit(EXIT_FAILURE);}
+   
+   if(power(4,2) != power(2,4))
+     {printf("broken6 implementation");
+       exit(EXIT_FAILURE);}
+
+   if(power(2,3)==9)
+     {printf("broken7 implementation");
+       exit(EXIT_FAILURE);}
+   
+   if(power(2, -2) == 4)
+     {printf("broken8 implementation");
+       exit(EXIT_FAILURE);}
+   
+   if(power(256, 4) != 4294967296)
+     {printf("broken10 implementation");
+       exit(EXIT_FAILURE);}
+
+   if(power(256, 2) != 65536)
+     {printf("broken9 implementation");
+       exit(EXIT_FAILURE);}
+
+   if(power(2,32 ) != 4294967296)
+     {printf("broken11 implementation");
+       exit(EXIT_FAILURE);}
+   return EXIT_SUCCESS;
+}
