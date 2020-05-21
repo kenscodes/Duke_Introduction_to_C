e490a4400aae5a62e2a182776cd370c12545ff82
Coursera Learner <nobody@nowhere.nul>
2020-05-21 17:15:35 +0000

    did changes

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -58,7 +58,62 @@ void assert_full_deck(deck_t * d) {
 
 }
 
+void add_card_to(deck_t * deck, card_t c){
+  deck->cards=realloc(deck->cards,(deck->n_cards+1)*sizeof(*deck->cards));
+  assert(deck->cards!=NULL);
+  deck->n_cards++;
+  deck->cards[deck->n_cards-1]=malloc(sizeof(**deck->cards));
+  assert(deck->cards[deck->n_cards-1]!=NULL);
+  deck->cards[deck->n_cards-1]->value=c.value;
+  deck->cards[deck->n_cards-1]->suit=c.suit;
+}
 
+card_t * add_empty_card(deck_t * deck){
+  card_t c={0,NUM_SUITS};
+  add_card_to(deck,c);
+  return deck->cards[deck->n_cards-1];
+}
+
+deck_t * make_deck_exclude(deck_t * excluded_cards){
+  deck_t *ans=malloc(sizeof(*ans));
+  assert(ans!=NULL);
+  ans->cards=NULL;
+  ans->n_cards=0;
+  for(unsigned i=0;i<52;i++){
+    card_t c=card_from_num(i);
+    if(!deck_contains(excluded_cards,c)){
+      add_card_to(ans,c);
+    }
+  }
+  return ans;
+}
+
+void free_deck(deck_t * deck);
+
+deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands){
+  deck_t *excluded=malloc(sizeof(*excluded));
+  assert(excluded!=NULL);
+  excluded->cards=NULL;
+  excluded->n_cards=0;
+  for(size_t i=0;i<n_hands;i++){
+    for(size_t j=0;j<hands[i]->n_cards;j++){
+      add_card_to(excluded,*hands[i]->cards[j]);
+    }
+  }
+  deck_t *ans=make_deck_exclude(excluded);
+  free_deck(excluded);
+  return ans;
+}
+
+void free_deck(deck_t * deck){
+  if(deck!=NULL){
+    for(size_t i=0;i<deck->n_cards;i++){
+      free(deck->cards[i]);
+    }
+    free(deck->cards);
+    free(deck);
+  }
+}
 
 
 
